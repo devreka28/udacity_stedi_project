1a9fe8e9717608f50a9a6683908b2e3029e9541c
devreka28 <50713651+devreka28@users.noreply.github.com>
2024-07-24 14:37:22 +0100

    Update customer_trusted_to_curated.py

--- customer_trusted_to_curated.py
+++ customer_trusted_to_curated.py
@@ -27,15 +27,15 @@ AccelerometerLanding_node1721825048304 = glueContext.create_dynamic_frame.from_o
 # Script generated for node Join customer trusted and accelerometer landing
 Joincustomertrustedandaccelerometerlanding_node1721825114474 = Join.apply(frame1=AccelerometerLanding_node1721825048304, frame2=CustomerTrusted_node1721824944773, keys1=["user"], keys2=["email"], transformation_ctx="Joincustomertrustedandaccelerometerlanding_node1721825114474")
 
-# Script generated for node Keep only customer fields
+# Script generated for node Keep only distinct customer fields
 SqlQuery0 = '''
-select serialNumber, sharewithpublicasofdate, birthday, registrationdate,sharewithresearchasofdate,customername,email,lastupdatedate, phone, sharewithfriendsasofdate from myDataSource
+select distinct serialNumber, sharewithpublicasofdate, birthday, registrationdate,sharewithresearchasofdate,customername,email,lastupdatedate, phone, sharewithfriendsasofdate from myDataSource
 '''
-Keeponlycustomerfields_node1721825188635 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":Joincustomertrustedandaccelerometerlanding_node1721825114474}, transformation_ctx = "Keeponlycustomerfields_node1721825188635")
+Keeponlydistinctcustomerfields_node1721825188635 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":Joincustomertrustedandaccelerometerlanding_node1721825114474}, transformation_ctx = "Keeponlydistinctcustomerfields_node1721825188635")
 
 # Script generated for node Customer Curated
 CustomerCurated_node1721825294358 = glueContext.getSink(path="s3://udacitycoursero3/customer/curated/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="CustomerCurated_node1721825294358")
 CustomerCurated_node1721825294358.setCatalogInfo(catalogDatabase="udacity_stedi_project",catalogTableName="customer_curated")
 CustomerCurated_node1721825294358.setFormat("json")
-CustomerCurated_node1721825294358.writeFrame(Keeponlycustomerfields_node1721825188635)
+CustomerCurated_node1721825294358.writeFrame(Keeponlydistinctcustomerfields_node1721825188635)
 job.commit()
