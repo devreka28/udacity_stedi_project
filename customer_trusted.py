00ab054de1993b34ad57aadf355bc57de69da9a3
devreka28 <50713651+devreka28@users.noreply.github.com>
2024-07-24 13:17:37 +0100

    Create customer_trusted.py

---
+++ customer_trusted.py
@@ -0,0 +1,35 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue import DynamicFrame
+
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Script generated for node Customer Landing
+CustomerLanding_node1721820485789 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://udacitycoursero3/customer/landing/"], "recurse": True}, transformation_ctx="CustomerLanding_node1721820485789")
+
+# Script generated for node Research agreed only records
+SqlQuery0 = '''
+select * from myDataSource where shareWithResearchAsOfDate is not null;
+'''
+Researchagreedonlyrecords_node1721820801370 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"myDataSource":CustomerLanding_node1721820485789}, transformation_ctx = "Researchagreedonlyrecords_node1721820801370")
+
+# Script generated for node Customer Trusted
+CustomerTrusted_node1721820967017 = glueContext.getSink(path="s3://udacitycoursero3/customer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="CustomerTrusted_node1721820967017")
+CustomerTrusted_node1721820967017.setCatalogInfo(catalogDatabase="udacity_stedi_project",catalogTableName="customer_trusted")
+CustomerTrusted_node1721820967017.setFormat("json")
+CustomerTrusted_node1721820967017.writeFrame(Researchagreedonlyrecords_node1721820801370)
+job.commit()
